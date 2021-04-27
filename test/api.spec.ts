b7794fe9a43d662a3efafb337699df8061e76230
Josh Peak <neozenith.dev@gmail.com>
2021-04-27 16:42:24 +1000

    Validate output

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -12,13 +12,14 @@ beforeAll(async () => {
 describe('GET /', () => {
   it('Normal mix of integers', async done => {
     // Given
-    const input: number[] = [1,2,3,4,5];
+    const input: number[] = [7,7,11,7,7];
     const query: string = input.join(',');
 
     request(target)
       .get(`/?input=${query}`)
       .expect(200)
       .expect('Content-Type', /json/)
+      .expect({ output: 7.8 })
       .end((err, res) => {
         if (err) return done(err);
         done();
